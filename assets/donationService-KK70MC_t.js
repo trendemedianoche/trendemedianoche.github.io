59500301d8d1f8d4be8c5e1a8cdc15b1181a1682
Nelson Alegria <nelson.alegria@femsasalud.com>
2025-12-31 16:24:10 -0300

    Updates

--- assets/donationService-KK70MC_t.js
+++ assets/donationService-KK70MC_t.js
@@ -1,4 +1,4 @@
-import{s as r}from"./index-CFPa9o6i.js";async function o(){const{data:e,error:t}=await r.from("donation_methods").select(`
+import{s as r}from"./index-DmwBOvo4.js";async function o(){const{data:e,error:t}=await r.from("donation_methods").select(`
       id,
       type,
       donation_transfer_data (
