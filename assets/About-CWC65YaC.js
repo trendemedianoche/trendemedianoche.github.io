482e897ad758d4f121b2fcf06d0efac27cb136bb
Nelson Alegria <nelson.alegria@femsasalud.com>
2026-01-22 00:48:16 -0300

    Updates

---
+++ assets/About-CWC65YaC.js
@@ -0,0 +1,1 @@
+import{r as t,j as e}from"./index-BdLs0ZPF.js";import{g as s}from"./aboutService-Bs8lw5fe.js";function u(){const[o,n]=t.useState("");return t.useEffect(()=>{s().then(n)},[]),e.jsx("section",{id:"about",children:e.jsx("div",{className:"about-container",dangerouslySetInnerHTML:{__html:o}})})}export{u as default};
